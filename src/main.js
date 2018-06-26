279784091bffca17572a368ca6e85bbf0b085b68
onelong <love@onelong.org>
2018-06-26 15:24:32 +0800

    fix: 修复一些样式

--- src/main.js
+++ src/main.js
@@ -9,31 +9,43 @@ function addListener(element, type, callback) {
   }
 }
 
+function clickNav() {
+  document.getElementById("menu_list").classList.add("hidden")
+}
+
 addListener(document, 'DOMContentLoaded', function () {
-  
+  var hasAddEvent
   var mq = window.matchMedia("(max-width: 1000px)")
   if (mq.matches) {
     document.getElementById("menu_list").classList.add("hidden");
     document.getElementById("menu_button").classList.remove("hidden")
+    hasAddEvent = true
+    addListener(document.getElementById("menu_list"), 'click', clickNav)
   }
-  
+
   addListener(document.getElementById("menu_button"), 'click', function () {
     document.getElementById("menu_list").classList.toggle("hidden")
   })
-  
+
   addListener(window, 'resize', function () {
     var width = window.innerWidth ||
-          document.documentElement.clientWidth ||
-          document.body.clientWidth
-    
+      document.documentElement.clientWidth ||
+      document.body.clientWidth
     if (width > 1000) {
       document.getElementById("menu_list").classList.remove("hidden")
       document.getElementById('menu_button').classList.add('hidden')
+      if (hasAddEvent) {
+        document.getElementById('menu_list').removeEventListener('click', clickNav)
+        hasAddEvent = false
+      }
     } else {
       document.getElementById("menu_list").classList.add("hidden")
       document.getElementById("menu_button").classList.remove("hidden")
-      
+      if (!hasAddEvent) {
+        addListener(document.getElementById("menu_list"), 'click', clickNav)
+        hasAddEvent = true
+      }
     }
   })
-  
+
 })
